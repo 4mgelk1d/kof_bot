6cc621c81d18c2c3ae7efca44b3e27e00c970931
4mgelk1d <markzeleny4@gmail.com>
2026-06-05 23:16:17 +0500

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-# ===== НАСТРОЙКИ - ОБЯЗАТЕЛЬНО ЗАМЕНИ =====
-BOT_TOKEN = "7958403209:AAGQPN_ZvRButSZYWibDx8ovH4_ofaTSXIA"
+BOT_TOKEN = "8924285335:AAFdPfErLdSSi9a2soS8_LaazeUWTK1mH00"
 OWNER_ID = 5584463063
 ADMIN_IDS = [5584463063]
-# =========================================
 
 DATABASE_PATH = "copy_bot.db"
 MAX_PROFILES = 10
